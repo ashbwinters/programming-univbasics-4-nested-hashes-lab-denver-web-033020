1351897e4f790c79c6f624a20c43130e19962ffe
Ashley Winters <ashbwinters@gmail.com>
2020-03-14 23:37:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-  programmer_hash[:grace_hopper]
+  return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,8 @@ def alan_kay_is_known_for
       }
     }
     
- 	programmer_hash[:alan_kay][:known_for]
+ 	return programmer_hash[:alan_kay][:known_for]
+ 	
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,9 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-  programmer_hash[:dennis_ritchie][:languages]
+    
+  return programmer_hash[:dennis_ritchie][:languages]
+  
 end
 
 def adding_matz
@@ -81,12 +84,14 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
 programmer_hash[:yukihiro_matsumoto] = {
   :known_for => "Ruby",
   :languages => ["LISP", "C"]
-  
-programmer_hash
 }
+  
+returnrn programmer_hash
+
 end
 
 def changing_alan
@@ -111,7 +116,7 @@ def changing_alan
     
   programmer_hash[:alan_kay][:known_for] = "GUI"
   
-  programmer_hash
+  return programmer_hash
   
 end
 
@@ -137,6 +142,6 @@ def adding_to_dennis
     
     programmer_hash[:dennis_ritchie][:languages] += "Assembly"
     
-    programmer_hash
+    return programmer_hash
 
 end
